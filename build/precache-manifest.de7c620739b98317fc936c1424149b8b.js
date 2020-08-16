3dd35e717a950ad786c12cc00124457535f10152
dgm1102 <62737392+dgm1102@users.noreply.github.com>
2020-08-16 11:20:13 -0700

    Add files via upload
    Robo app

---
+++ build/precache-manifest.de7c620739b98317fc936c1424149b8b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "840f1940e17e0e8435c5e2fc47fa92f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7050068a4d1369b55ac",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "09d17ce43a758422c8c6",
+    "url": "/static/css/main.91355ebc.chunk.css"
+  },
+  {
+    "revision": "f7050068a4d1369b55ac",
+    "url": "/static/js/2.a0121e35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a0121e35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09d17ce43a758422c8c6",
+    "url": "/static/js/main.65cc1844.chunk.js"
+  },
+  {
+    "revision": "a7abf245035ac7a562b4",
+    "url": "/static/js/runtime-main.cd203117.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
